bec6cc09032d64ec40ffcd30b2ea6c05f2c6691c
Shivani Cheela <155736097+shivanicheela@users.noreply.github.com>
2026-05-24 19:36:30 +0530

    Fix typo in main check for run.py

--- backend/run.py
+++ backend/run.py
@@ -11,7 +11,7 @@ To run the backend:
 import uvicorn
 from app.main import app
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     import os, uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app.main:app", host="0.0.0.0", port=port, log_level="info")
